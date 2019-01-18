8c3c3c89601789f684b48c542195d0f23a6e4d39
Jasper <bobmugura@gmail.com>
2019-01-17 23:40:18 -0500

    added api data fetching

--- app/client/src/components/ListsContainer.js
+++ app/client/src/components/ListsContainer.js
@@ -1,13 +1,43 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 
 class ListsContainer extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      lists: []
+    }
+  }
+
+  componentDidMount() {
+    axios.get('http://localhost:3001/api/v1/lists')
+      .then(response => {
+        console.log(response)
+        this.setState({
+          lists: response.data
+        })
+      })
+      .catch(error => console.log(error))
+  }
+
   render() {
     return (
       <div className="Lists-container">
-        Lists
+        {this.state.lists.map(list => {
+          return (
+            <div className="single-list" key={list.id}>
+              <h4>{list.title}</h4>
+              <p>{list.excerpt}</p>
+            </div>
+          )
+        })
+
+        }
       </div>
     )
   }
+
 }
 
 export default ListsContainer
