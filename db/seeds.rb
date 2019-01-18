2aba217e0ebd9cd81f695e679c4e1b4113e1eafe
Jasper <bobmugura@gmail.com>
2019-01-17 21:41:00 -0500

    migrated db and seeded data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# From http://jsonplaceholder.typicode.com/posts
+List.create(title: 'sunt aut facere repellat provident occaecati excepturi optio reprehenderit', excerpt: 'quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto')
+List.create(title: 'qui est esse', excerpt: 'est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla')
+List.create(title: 'ea molestias quasi exercitationem repellat qui ipsa sit aut', excerpt: 'et iusto sed quo iure\nvoluptatem occaecati omnis eligendi aut ad\nvoluptatem doloribus vel accusantium quis pariatur\nmolestiae porro eius odio et labore et velit aut')
