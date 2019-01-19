5bfe2cb29109d23a6af603f01b18834439538e7f
Jasper <bobmugura@gmail.com>
2019-01-19 15:39:22 -0500

    added CRUD: Update

--- app/client/src/components/ListsContainer.js
+++ app/client/src/components/ListsContainer.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react'
 import axios from 'axios'
 import List from './List'
-import NewListForm from './NewListForm';
+import NewListForm from './NewListForm'
+import EditListForm from './EditListForm'
 
 class ListsContainer extends Component {
 
   constructor(props) {
     super(props)
     this.state = {
-      lists: []
+      lists: [],
+      editingListId: null
     }
   }
 
@@ -35,6 +37,31 @@ class ListsContainer extends Component {
       .catch(error => console.log(error))
   }
 
+  editingList = (id) => {
+    this.setState({
+      editingListId: id
+    })
+  }
+
+  editList = (id, title, excerpt) => {
+    axios.put('/api/v1/lists/' + id, {
+      list: {
+        title,
+        excerpt
+      }
+    })
+      .then(response => {
+        console.log(response)
+        const lists = this.state.lists
+        lists[id - 1] = { id, title, excerpt }
+        this.setState(() => ({
+          lists,
+          editingListId: null
+        }))
+      })
+      .catch(error => console.log(error))
+  }
+
   componentDidMount() {
     axios.get('http://localhost:3001/api/v1/lists')
       .then(response => {
@@ -46,15 +73,34 @@ class ListsContainer extends Component {
       .catch(error => console.log(error))
   }
 
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.editingListId !== this.state.editingListId) {
+      axios.get('http://localhost:3001/api/v1/lists')
+        .then(response => {
+          console.log(response)
+          this.setState({
+            lists: response.data
+          })
+        })
+        .catch(error => console.log(error))
+    }
+  }
+
   render() {
     return (
       <div className="Lists-container">
         <NewListForm onNewList={this.addNewList} />
         {
           this.state.lists.map(list => {
-            return (
-              <List list={list} key={list.id} onRemoveList={this.removeList} />
-            )
+            if (this.state.editingListId === list.id) {
+              return (
+                <EditListForm list={list} key={list.id} editList={this.editList} />
+              )
+            } else {
+              return (
+                <List list={list} key={list.id} onRemoveList={this.removeList} editingList={this.editingList} />
+              )
+            }
           })
         }
       </div>
